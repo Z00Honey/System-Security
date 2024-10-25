a18fd176683b5a8c589bd43948166bd5c0315525
me2nuk <57348147+me2nuk@users.noreply.github.com>
2024-10-26 00:04:28 +0900

    fix: image path

--- src/utils/load.py
+++ src/utils/load.py
@@ -2,10 +2,10 @@ from PyQt5.QtCore import QFile, QTextStream
 import os, sys
 
 def image_base_path(filename):
-    return os.path.join(os.getcwd() + "\\assets\\images\\", filename)
+    return os.path.join(os.getcwd() + "\\src\\assets\\images\\", filename)
 
 def load_stylesheet(filename):
-    stylesheet_base_path = os.path.join(os.getcwd() + "\\assets\\css\\")
+    stylesheet_base_path = os.path.join(os.getcwd() + "\\src\\assets\\css\\")
     
     qss_filename = stylesheet_base_path + filename
     qss_file = QFile(qss_filename)
