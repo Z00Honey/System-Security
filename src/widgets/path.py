80cc53783194dc128b5c2b5aa2cd07c5ee5a4c29
Myyoung0 <165647242+Myyoung0@users.noreply.github.com>
2024-11-04 01:08:44 +0900

    Comment: unnecessary  comment delete

--- src/widgets/path.py
+++ src/widgets/path.py
@@ -11,24 +11,20 @@ class PathBar(QWidget):
         self.layout.setContentsMargins(10, 0, 10, 0)
         self.layout.setSpacing(5)
 
-        # 사용자의 홈 디렉토리 경로 가져오기
         user_home_path = os.path.expanduser("~")
         self.path_label = QLabel(user_home_path)
-        self.path_label.setObjectName("path_label")  # QLabel에 이름을 설정합니다.
+        self.path_label.setObjectName("path_label") 
 
-        # QLabel의 높이를 설정하여 중앙 정렬 보장
-        self.path_label.setFixedHeight(40)  # 필요에 따라 높이를 조정
+        self.path_label.setFixedHeight(40) 
 
-        # 레이아웃에 QLabel 추가
         self.layout.addWidget(self.path_label)
-        self.layout.setAlignment(self.path_label, Qt.AlignCenter)  # QLabel을 중앙 정렬
+        self.layout.setAlignment(self.path_label, Qt.AlignCenter)  
 
         self.setLayout(self.layout)
         self.setStyleSheet(load_stylesheet("path.css"))
 
-    # 경로 업데이트 메서드
     def update_path(self, new_path: str):
-        self.path_label.setText(new_path)  # 업데이트된 경로만 표시
+        self.path_label.setText(new_path) 
 
 
 
