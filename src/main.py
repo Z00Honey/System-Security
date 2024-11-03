1deb1aebf024b652e3a8e73f2b318b49d4e421a3
purplesky <pwned@korea.edu>
2024-11-03 09:58:39 +0900

    fix: resolve merge conflict
    resolved git conflict during feat/ui -> base

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-from gui.context import main
+from PyQt5.QtWidgets import *
+from window import MainWindow
+import sys
 
-if __name__ == "__main__":
-    main()
+sys.dont_write_bytecode = True
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
