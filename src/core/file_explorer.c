ff3534d75c8e3ec86dcb23caa1479cd69d530260
andsopwn <pwned@korea.edu>
2024-11-23 16:30:12 +0900

    remove: legacy

--- src/core/file_explorer.c
+++
@@ -1,6 +0,0 @@
-﻿#include <windows.h>
-#include <stdio.h>
-
-void explore_directory(const char *path);
-void create_file(const char *path, const char *filename);
-void delete_file(const char *path, const char *filename);
--- src/core/include/file_explorer.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILE_EXPLORER_H
-#define FILE_EXPLORER_H
-
-void list_files(const char* directory);
-
-#endif
