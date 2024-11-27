22b958c8078d56b007293bcdffa631b5144ba478
andsopwn <pwned@korea.edu>
2024-11-27 11:32:57 +0900

    fixed: file_system_model restore

--- src/models/file_system_model.py
+++ src/models/file_system_model.py
@@ -8,23 +8,11 @@ class FileExplorerModel(QFileSystemModel):
         self.setRootPath("")
         self.setFilter(QDir.AllEntries | QDir.NoDotAndDotDot)
         self._headers = ["Name", "Date Modified", "Type", "Size"]
-
-        self.setReadOnly(False)
         
-    def columnCount(self, parent=None):
-        return 4  # 이름, 크기, 유형, 수정한 날짜
-        
-    def headerData(self, section, orientation, role=Qt.DisplayRole):
+    def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            if section == 0:
-                return "이름"
-            elif section == 1:
-                return "크기"
-            elif section == 2:
-                return "유형"
-            elif section == 3:
-                return "수정한 날짜"
-        return super().headerData(section, orientation, role) 
+            return self._headers[section]
+        return super().headerData(section, orientation, role)
         
     def data(self, index, role):
         if role == Qt.DisplayRole:
