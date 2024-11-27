bc14ca1b1f77f7fd5a5803f8d6d9e2c444228685
Myyoung0 <165647242+Myyoung0@users.noreply.github.com>
2024-11-27 21:13:43 +0900

    design: file information file count add

--- src/widgets/file_information.py
+++ src/widgets/file_information.py
@@ -1,7 +1,9 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QFrame, QHBoxLayout
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
-from utils.load import image_base_path
+from collections import defaultdict
+from utils.load import image_base_path, load_stylesheet
+import os
 
 class FileInformation(QWidget):
     def __init__(self, parent=None):
@@ -27,8 +29,10 @@ class FileInformation(QWidget):
         
         self.info_layout = QVBoxLayout()
         self.layout.addLayout(self.info_layout)
+        self.setStyleSheet(load_stylesheet("file_information.css")) 
+        self.setObjectName("file_information")
         
-        self.setFixedHeight(159)
+        self.setFixedHeight(180)
         self.hide()
 
     def add_horizontal_separator(self):
@@ -37,17 +41,47 @@ class FileInformation(QWidget):
         line.setFrameShadow(QFrame.Sunken)
         self.layout.addWidget(line)
 
+    def get_folder_contents_count(self, folder_path):
+        try:
+            items = os.listdir(folder_path)
+            file_count = len([item for item in items if os.path.isfile(os.path.join(folder_path, item))])
+            folder_count = len([item for item in items if os.path.isdir(os.path.join(folder_path, item))])
+            return file_count, folder_count
+        except:
+            return 0, 0
+
+    def get_file_types(self, folder_path):
+        try:
+            items = os.listdir(folder_path)
+            file_types = defaultdict(int)
+            
+            for item in items:
+                item_path = os.path.join(folder_path, item)
+                if os.path.isfile(item_path):
+                    _, ext = os.path.splitext(item)
+                    if ext:
+                        file_types[ext.upper()] += 1
+            return dict(file_types)
+        except:
+            return {}
+
     def show_file_info(self, file_info):
-        # 기존 정보 클리어
         for i in reversed(range(self.info_layout.count())): 
             self.info_layout.itemAt(i).widget().deleteLater()
             
-        # 새 정보 추가
         for key, value in file_info.items():
-            info_label = QLabel(f"{key}: {value}")
-            self.info_layout.addWidget(info_label)
-        
-        self.show()  # 정보와 함께 위젯 표시
+            if key == "유형" and value == "폴더":
+                info_label = QLabel(f"{key}: {value}")
+                self.info_layout.addWidget(info_label)
+            
+                file_count, folder_count = self.get_folder_contents_count(file_info["경로"])
+                contents_label = QLabel(f"포함된 항목: 파일 {file_count}개, 폴더 {folder_count}개")
+                self.info_layout.addWidget(contents_label)
+            else:
+                info_label = QLabel(f"{key}: {value}")
+                self.info_layout.addWidget(info_label)
+    
+            self.show()
 
     def clear_info(self):
         self.hide()
